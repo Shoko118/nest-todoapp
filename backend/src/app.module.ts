493f0db02c470ebee7b8f75bdd1faa92e1a5a868
Bryan Liaw <bryanliaw@Bryans-MacBook-Pro.local>
2024-05-01 18:45:24 +0800

    config typeorm modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,20 +7,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Todo } from './todos/todo.entity';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    TodosModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: String(process.env.DB_HOST),
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      entities: [Todo],
-      synchronize: true,
-    }),
-  ],
+  imports: [ConfigModule.forRoot(), TodosModule, TypeOrmModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ backend/src/datasource/typeorm.module.ts
@@ -0,0 +1,35 @@
+import { DataSource } from 'typeorm';
+import { Global, Module } from '@nestjs/common';
+
+@Global() // makes the module available globally for other modules once imported in the app modules
+@Module({
+  imports: [],
+  providers: [
+    {
+      provide: DataSource, // add the datasource as a provider
+      inject: [],
+      useFactory: async () => {
+        try {
+          const dataSource = new DataSource({
+            type: 'postgres',
+            host: String(process.env.DB_HOST),
+            port: Number(process.env.DB_PORT),
+            username: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE,
+            // entities: [Todo],
+            synchronize: true,
+            entities: [`${__dirname}/../**/**.entity{.ts,.js}`], // this will automatically load all entity file in the src folder
+          });
+          await dataSource.initialize(); // initialize the data source
+          console.log('Database connected successfully');
+          return dataSource;
+        } catch (error) {
+          console.log('Error connecting to database');
+        }
+      },
+    },
+  ],
+  exports: [DataSource],
+})
+export class TypeOrmModule {}
